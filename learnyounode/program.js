1f4925581626a92444bbad3cbb42db5a688c193f
Harry Moreno <morenoh149@gmail.com>
2014-06-06 15:32:12 -0700

    finished problem 12

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,15 +1,1 @@
-// problem 11 http file server
-var fs = require('fs');
-var http = require('http');
-var port = process.argv[2];
-var filepath = process.argv[3];
-
-var server = http.createServer(function(req, res) {
-  fs.createReadStream(filepath).pipe(res);
-});
-
-server.listen(port);
-
-//solution also contains
-//res.writeHead(200, { 'content-type': 'text/plain' })
-//before piping file
+// problem 1
